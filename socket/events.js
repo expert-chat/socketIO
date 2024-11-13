c773f18c20c490a70d1b28e2a30f20ef53c4d60d
m <m@m.com>
2024-11-13 20:25:10 +0200

    chat

--- socket/events.js
+++ socket/events.js
@@ -3,27 +3,27 @@ const users = {};
 
 export default function registerEvents(socket, io) {
     // Parse the user JSON data from the query params
-    // const user = JSON.parse(socket?.handshake?.query?.user);
-    //
-    // if (user?.id) {
-    //     // Check if the user is already in the users object
-    //     if (users[user.id] && users[user.id].disconnectTimeout) {
-    //         // Clear any pending disconnection if the user reconnects within 5 seconds
-    //         clearTimeout(users[user.id].disconnectTimeout);
-    //         console.log(`User ${user.name} reconnected. Cancelled disconnect timeout.`);
-    //     }
-    //
-    //     // Store or update the user's socketId and clear disconnectTimeout
-    //     users[user.id] = {
-    //         socketId: socket.id,
-    //         disconnectTimeout: null,
-    //     };
-    //
-    //     const message = `User (${user.name}) connected with socket ID: (${socket.id})`;
-    //     console.log(message);
-    //
-    //     socket.broadcast.emit('userConnected', message);
-    // }
+    const user = JSON.parse(socket?.handshake?.query?.user);
+
+    if (user?.id) {
+        // Check if the user is already in the users object
+        if (users[user.id] && users[user.id].disconnectTimeout) {
+            // Clear any pending disconnection if the user reconnects within 5 seconds
+            clearTimeout(users[user.id].disconnectTimeout);
+            console.log(`User ${user.name} reconnected. Cancelled disconnect timeout.`);
+        }
+
+        // Store or update the user's socketId and clear disconnectTimeout
+        users[user.id] = {
+            socketId: socket.id,
+            disconnectTimeout: null,
+        };
+
+        const message = `User (${user.name}) connected with socket ID: (${socket.id})`;
+        console.log(message);
+
+        socket.broadcast.emit('userConnected', message);
+    }
 
     socket.on('saveParticipants', (participants) => {
         const authId = user?.id;
@@ -47,15 +47,20 @@ export default function registerEvents(socket, io) {
     });
 
 
+    socket.on('sendMessage', (data) => {
+        console.dir({data});
+    });
+
+
     // Handle user disconnection with a delay
     socket.on('disconnect', () => {
-        // if (user?.id && users[user.id]) {
-        //     users[user.id].disconnectTimeout = setTimeout(() => {
-        //         console.log(`User ${user.id} disconnected (Socket ID: ${socket.id}) after delay.`);
-        //         socket.broadcast.emit('userDisconnected', `User with ID: (${user.id}) has disconnected.`);
-        //         delete users[user.id];
-        //     }, 5000);
-        // }
+        if (user?.id && users[user.id]) {
+            users[user.id].disconnectTimeout = setTimeout(() => {
+                console.log(`User ${user.id} disconnected (Socket ID: ${socket.id}) after delay.`);
+                socket.broadcast.emit('userDisconnected', `User with ID: (${user.id}) has disconnected.`);
+                delete users[user.id];
+            }, 5000);
+        }
     });
 }
 
