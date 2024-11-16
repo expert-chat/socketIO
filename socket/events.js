efd58bce4d0c805066b2de1fe27a417bc10af267
m <m@m.com>
2024-11-17 00:41:32 +0200

    sar3

--- socket/events.js
+++ socket/events.js
@@ -1,28 +1,11 @@
-const users = new Map();
+export default function registerEvents(socket, io, users, user) {
 
-export default function registerEvents(socket, io) {
-
-    const user = JSON.parse(socket?.handshake?.query?.user);
-
-    if (user?.id) {
-        if (users.has(user.id)) {
-            const existingUser = users.get(user.id);
-            if (existingUser.disconnectTimeout) {
-                clearTimeout(existingUser.disconnectTimeout);
-                console.log(`User ${user.name} reconnected. Cancelled disconnect timeout.`);
-            }
-        }
-
-        users.set(user.id, {
-            socketId: socket.id,
-            disconnectTimeout: null,
-        });
-
-        console.log(`User (${user.name}) connected with socket ID: (${socket.id})`);
+    socket.on('iamOnline', () => {
+        console.log(`Connected:(${user.name}) connected with socket ID: (${socket.id})`);
         socket.broadcast.emit('userConnected', `User (${user.name}) connected.`);
-    }
+    });
 
-    socket.on('setParticipants', (participants) => {
+    socket.on('getOnlineParticipants', (participants) => {
         const authId = user?.id;
 
         if (authId && users.has(authId)) {
@@ -34,13 +17,10 @@ export default function registerEvents(socket, io) {
 
     socket.on('disconnect', () => {
         if (user?.id && users.has(user.id)) {
-            const disconnectTimeout = setTimeout(() => {
-                console.log(`User ${user.id} disconnected (Socket ID: ${socket.id}) after delay.`);
-                socket.broadcast.emit('userDisconnected', `User (${user.id}) disconnected.`);
-                users.delete(user.id);
-            }, 5000);
-
-            users.set(user.id, {...users.get(user.id), disconnectTimeout});
+            console.log(`Disconnected: ${user.id}`);
+            socket.broadcast.emit('offline', user);
+            users.delete(user.id);
         }
     });
+
 }
--- socket/index.js
+++ socket/index.js
@@ -2,18 +2,29 @@ import { Server } from 'socket.io';
 import registerEvents from './events.js';
 import { authenticate } from './middleware.js';
 
+const users = new Map();
+
+
 export default function socketInit(server) {
     const io = new Server(server, {
         cors: {
-            origin: "*",  // Allow all origins; adjust for production
+            origin: "*",
             methods: ["GET", "POST"]
         }
     });
 
-    io.use(authenticate); // Apply middleware
+    io.use(authenticate);
 
     io.on('connection', (socket) => {
-        registerEvents(socket, io); // Register events
+
+        const user = JSON.parse(socket?.handshake?.query?.user);
+
+        if (user?.id) {
+            users.set(user.id, {socketId: socket.id});
+            socket.broadcast.emit('online', user);
+        }
+
+        registerEvents(socket, io, users, user); // Register events
     });
 
     io.on('disconnect', (socket) => {
