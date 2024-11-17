49d4d5b1e23a3d963ab3f350e3487442a420d514
Elnagar <m@m.com>
2024-11-17 17:01:24 +0200

    chats

--- socket/events.js
+++ socket/events.js
@@ -1,19 +1,41 @@
 export default function registerEvents(socket, io, users, user) {
 
-    socket.on('iamOnline', () => {
-        console.log(`Connected:(${user.name}) connected with socket ID: (${socket.id})`);
-        socket.broadcast.emit('userConnected', `User (${user.name}) connected.`);
-    });
-
+    // get my online participants
     socket.on('getOnlineParticipants', (participants) => {
         const authId = user?.id;
-
         if (authId && users.has(authId)) {
             const onlineParticipants = participants.filter(participant => users.has(participant));
-            console.log(`Online participants:`, onlineParticipants);
             socket.emit('onlineParticipants', onlineParticipants);
         }
     });
+
+
+    // send message to others
+    socket.on('messageSent', (message, chat) => {
+        const authId = user?.id;
+        const participants = chat?.participants;
+
+        console.log({participants});
+        if (authId && users.has(authId) && Array.isArray(participants)) {
+            participants.forEach((participant) => {
+                if (participant && users.has(participant.id)) {
+                    const targetSocket = users.get(participant.id);
+                    if (targetSocket["socketId"]) {
+                        console.log("======================");
+                        console.dir({participant});
+                        console.dir({chat});
+                        console.dir({message});
+                        console.dir({targetSocket: targetSocket["socketId"]});
+                        console.log("======================");
+                        socket.to(targetSocket["socketId"]).emit('haveNewMessage', message, chat);
+                    }
+                }
+            });
+        } else {
+            console.error("empty Participants");
+        }
+    });
+
 
     socket.on('disconnect', () => {
         if (user?.id && users.has(user.id)) {
