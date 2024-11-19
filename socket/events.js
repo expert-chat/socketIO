ab43a9e2de47930754d4f055f87612f2ec3e6962
m <m@m.com>
2024-11-20 00:17:58 +0200

    chat

--- socket/events.js
+++ socket/events.js
@@ -9,7 +9,6 @@ export default function registerEvents(socket, io, users, user) {
         }
     });
 
-
     // send message to others
     socket.on('messageSent', (message, chat) => {
         const authId = user?.id;
@@ -23,6 +22,41 @@ export default function registerEvents(socket, io, users, user) {
                     const targetSocket = users.get(participant.id);
                     if (targetSocket["socketId"]) {
                         socket.to(targetSocket["socketId"]).emit('haveNewMessage', message, chat);
+                    }
+                }
+            });
+
+        } else {
+            console.error("empty Participants");
+        }
+    });
+
+    // send message read by
+    socket.on('messageReadBy', (auth, message) => {
+        const senderId = message?.sender?.id;
+
+        if (senderId && users.has(senderId)) {
+            const targetSocket = users.get(senderId);
+            socket.to(targetSocket["socketId"]).emit('haveReadMessage', auth, message);
+        } else {
+            console.error("empty senderId");
+        }
+    });
+
+
+    // send last chat message read by
+    socket.on('lastChatMessageReadBy', (auth, chat) => {
+        const authId = user?.id;
+        const participants = chat?.participants;
+
+        if (authId && users.has(authId) && Array.isArray(participants)) {
+
+            /* send to participants */
+            participants.forEach((participant) => {
+                if (participant && users.has(participant.id)) {
+                    const targetSocket = users.get(participant.id);
+                    if (targetSocket["socketId"]) {
+                        socket.to(targetSocket["socketId"]).emit('haveLastChatMessageRead', auth, chat);
                     }
                 }
             });
